a1022cec493ed9ed7eb533864359a6f190dc14f7
JulioOLV <julioccolv@gmail.com>
2022-11-23 17:40:47 -0300

    feat: add new tratatives

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,32 +1,52 @@
+import random
+
 print("*********************************")
 print("Bem vindo no jogo de Adivinhacao!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
 
-total_de_tentativas = 3
-rodada = 1
+print("(1) Facil (2) Medio (3) Dificil")
+nivel = int(input("Defina o nivel: "))
 
-while(rodada <= total_de_tentativas):
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    valor_informado = input("Digite o seu numero: ")
+    valor_informado = input("Digite um número entre 1 e 100: ")
 
     print("Voce digitou ", valor_informado)
 
-    number = int(valor_informado)
+    numero = int(valor_informado)
 
-    acertou = number == numero_secreto
-    maior   = number > numero_secreto
-    menor   = number < numero_secreto
+    if (numero < 1 or numero > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = numero == numero_secreto
+    maior   = numero > numero_secreto
+    menor   = numero < numero_secreto
 
     if(acertou):
-        print("Voce acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
+        break
     else:
         if(maior):
             print("Voce errou! O seu chute foi maior que o numero secreto.")
+            if (rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
         elif(menor):
             print("Voce errou! O seu chute foi menor que o numero secreto.")
-
-    rodada = rodada + 1
+            if (rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
+        pontos_perdidos = abs(numero_secreto - numero)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
