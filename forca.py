1d3358d2f16a112b5577267cb2edf9026abae760
JulioOLV <julioccolv@gmail.com>
2022-12-07 17:28:58 -0300

    feat: add string tratatives for uppercase and remove spaces

--- forca.py
+++ forca.py
@@ -9,6 +9,14 @@ def play():
     acertou = False
 
     while not enforcou and not acertou:
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posicao {}!".format(letra, index))
+            index = index + 1
         print("jogando...")
 
     print("Fim do jogo")
